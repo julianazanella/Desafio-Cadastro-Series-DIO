026b06507e64d2b1c37b37394e1bb7f133798192
Juliana Zanella <96039882+julianazanella@users.noreply.github.com>
2022-01-03 18:04:19 -0300

    Add files via upload
    Projeto PetPlay

---
+++ Genero.cs
@@ -0,0 +1,15 @@
+namespace PetPlay
+{
+	public enum Genero
+	{
+		Acao = 1,
+		Aventura = 2,
+		Comedia = 3,
+		Documentario = 4,
+		Drama = 5,
+		Fantasia = 6,
+		Ficcao_Cientifica = 9,
+		Musical = 10,
+		Romance = 11,
+	}
+}
